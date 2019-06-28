a85ab2fca0b64250c91b32952bfb3ffec2c8cfc1
Josema <enzo@josema.es>
2019-06-28 16:47:10 +0200

    Updated runandrisk dependencies

--- src/const/parameters.js
+++ src/const/parameters.js
@@ -1,8 +1,8 @@
 const GAME = {
     CRON_INTERVAL: 500, // ms
-    MIN_PLAYERS: 2,
     MAX_PLAYERS: 4,
-    GAME_STARTS_AT: 30, // seconds
+    MIN_PLAYERS: 4,
+    GAME_STARTS_AT: 1, // seconds
     GAME_ENDS_AT: 10 * 60, // minutes
     NEXT_RECRUITMENT: 2 * 60, // minutes
     STOP_RECRUITMENT: 20,
